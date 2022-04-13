e1bf60c8ffc6834fa01313576c767f7f5f05b62f
Paulo Monezi <paulo.monezi@icloud.com>
2022-04-13 14:25:44 -0400

    initial structure added

--- visualizador-de-produtos/Form1.Designer.cs
+++ visualizador-de-produtos/Form1.Designer.cs
@@ -28,13 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.BrandCBox = new System.Windows.Forms.ComboBox();
+            this.ModelCBox = new System.Windows.Forms.ComboBox();
+            this.BrandLabel = new System.Windows.Forms.Label();
+            this.ModelLabel = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BrandCBox
+            // 
+            this.BrandCBox.FormattingEnabled = true;
+            this.BrandCBox.Items.AddRange(new object[] {
+            "CHEVROLET",
+            "FIAT",
+            "FORD"});
+            this.BrandCBox.Location = new System.Drawing.Point(140, 122);
+            this.BrandCBox.Name = "BrandCBox";
+            this.BrandCBox.Size = new System.Drawing.Size(121, 21);
+            this.BrandCBox.TabIndex = 1;
+            this.BrandCBox.SelectedIndexChanged += new System.EventHandler(this.BrandCBox_SelectedIndexChanged);
+            // 
+            // ModelCBox
+            // 
+            this.ModelCBox.FormattingEnabled = true;
+            this.ModelCBox.Location = new System.Drawing.Point(140, 175);
+            this.ModelCBox.Name = "ModelCBox";
+            this.ModelCBox.Size = new System.Drawing.Size(121, 21);
+            this.ModelCBox.TabIndex = 2;
+            this.ModelCBox.SelectedIndexChanged += new System.EventHandler(this.ModelCBox_SelectedIndexChanged);
+            // 
+            // BrandLabel
+            // 
+            this.BrandLabel.AutoSize = true;
+            this.BrandLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BrandLabel.ForeColor = System.Drawing.Color.White;
+            this.BrandLabel.Location = new System.Drawing.Point(45, 122);
+            this.BrandLabel.Name = "BrandLabel";
+            this.BrandLabel.Size = new System.Drawing.Size(46, 13);
+            this.BrandLabel.TabIndex = 3;
+            this.BrandLabel.Text = "Marca:";
+            // 
+            // ModelLabel
+            // 
+            this.ModelLabel.AutoSize = true;
+            this.ModelLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ModelLabel.ForeColor = System.Drawing.Color.White;
+            this.ModelLabel.Location = new System.Drawing.Point(45, 183);
+            this.ModelLabel.Name = "ModelLabel";
+            this.ModelLabel.Size = new System.Drawing.Size(52, 13);
+            this.ModelLabel.TabIndex = 4;
+            this.ModelLabel.Text = "Modelo:";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(311, 45);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(410, 343);
+            this.panel1.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(23, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(359, 296);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.ModelLabel);
+            this.Controls.Add(this.BrandLabel);
+            this.Controls.Add(this.ModelCBox);
+            this.Controls.Add(this.BrandCBox);
+            this.Name = "Form1";
+            this.Text = "Visualizador de Produtos";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox BrandCBox;
+        private System.Windows.Forms.ComboBox ModelCBox;
+        private System.Windows.Forms.Label BrandLabel;
+        private System.Windows.Forms.Label ModelLabel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
--- visualizador-de-produtos/Form1.cs
+++ visualizador-de-produtos/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,44 @@ namespace visualizador_de_produtos
 {
     public partial class Form1 : Form
     {
+        private const string PathPictures = @"C:\Users\pa_ul\source\repos\visualizador-de-produtos\fotos";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void ModelCBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string[] carPictures = Directory.GetFiles(PathPictures);
+
+            foreach(string picture in carPictures)
+            {
+                 
+            }
+        }
+
+        private void BrandCBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (BrandCBox.Text == "FIAT")
+            {
+                ModelCBox.Items.Clear();
+                ModelCBox.Items.Add("STRADA");
+                ModelCBox.Items.Add("PUNTO");
+                ModelCBox.Items.Add("ARGO");
+            }
+            else if (BrandCBox.Text == "FORD")
+            {
+                ModelCBox.Items.Clear();
+                ModelCBox.Items.Add("FOCUS");
+                ModelCBox.Items.Add("MUSTANG");
+                ModelCBox.Items.Add("FORDONA BRABA");
+            }
+            else if (BrandCBox.Text == "CHEVROLET")
+            {
+                ModelCBox.Items.Clear();
+                ModelCBox.Items.Add("ONIX");
+            }
         }
     }
 }
